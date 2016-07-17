3edfd79bee78eafcbbce325cbda9da8b41c0a2a3
ednodete <viktor_dimitrievski@hotmail.com>
2016-07-17 14:29:02 +0200

    Setting start up project

---
+++ MyTask/WepApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WepApp.Startup))]
+namespace WepApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
