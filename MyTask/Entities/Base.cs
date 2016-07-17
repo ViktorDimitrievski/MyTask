8e7d480f14f98585d36d8b5848489088cf35ad69
ednodete <viktor_dimitrievski@hotmail.com>
2016-07-17 14:55:28 +0200

    Add Entities

---
+++ MyTask/Entities/Base.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Base
+    {
+        public virtual int ID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual DateTime DateCreated { get; set; }
+        public virtual bool isActive { get; set; }
+    }
+}
